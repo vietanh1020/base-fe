9a3343b7bd24bcbe9c948b3bc3a5a60b2f9b3edc
AnhVV <anh.vv@zinza.com.vn>
2023-06-27 17:56:01 +0700

    add firebase push notify

---
+++ firebase.ts
@@ -0,0 +1,40 @@
+import firebase from "firebase/app";
+import "firebase/messaging";
+import { firebaseConfig } from "./constants";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+
+let messaging: firebase.messaging.Messaging;
+
+if (typeof window !== "undefined") {
+  if (firebase.messaging.isSupported()) {
+    messaging = firebase.messaging();
+  }
+}
+
+export const getMessagingToken = async () => {
+  let currentToken = "";
+  if (!messaging) return;
+  try {
+    currentToken = await messaging.getToken({
+      vapidKey: process.env.REACT_APP_FIREBASE_FCM_VAPID_KEY,
+    });
+    console.log("FCM registration token", currentToken);
+  } catch (error) {
+    console.log("An error occurred while retrieving token. ", error);
+  }
+  return currentToken;
+};
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    messaging.onMessage((payload) => {
+      console.log({ anhvv: payload });
+
+      resolve(payload);
+    });
+  });
