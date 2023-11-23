18bfb254450c12ec9c7b172f4d8fcc37b1811f6d
AnhVV <anh.vv@zinza.com.vn>
2023-11-23 11:53:21 +0700

    [WIP]

--- pages/customers.tsx
+++ pages/customers.tsx
@@ -1,7 +1,7 @@
 import { CustomersSearch } from "@/components/customers/CustomerSearch";
 import { CustomersTable } from "@/components/customers/CustomerTable";
 import { useSelection } from "@/hooks/useSelection";
-import { applyPagination } from "@/services/CustomerService";
+import { applyPagination, useGetUser } from "@/services/CustomerService";
 import ArrowDownOnSquareIcon from "@heroicons/react/24/solid/ArrowDownOnSquareIcon";
 import ArrowUpOnSquareIcon from "@heroicons/react/24/solid/ArrowUpOnSquareIcon";
 import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
@@ -16,127 +16,10 @@ import {
 import { subDays, subHours } from "date-fns";
 import { useCallback, useMemo, useState } from "react";
 
-const now = new Date();
-
-const data = [
-  {
-    id: "5e86809283e28b96d2d38537",
-    address: {
-      city: "Madrid",
-      country: "Spain",
-      name: "Anika Visser",
-      street: "4158  Hedge Street",
-    },
-    avatar: "https://htran844.github.io/hihi/img/ig/i42.jpg",
-    createdAt: subDays(subHours(now, 11), 2).getTime(),
-    email: "anika.visser@devias.io",
-    name: "Anika Visser",
-    phone: "908-691-3242",
-  },
-  {
-    id: "5e86805e2bafd54f66cc95c3",
-    address: {
-      city: "San Diego",
-      country: "USA",
-      state: "California",
-      street: "75247",
-    },
-    avatar: "https://htran844.github.io/hihi/img/ig/i41.jpg",
-    createdAt: subDays(subHours(now, 7), 3).getTime(),
-    email: "miron.vitold@devias.io",
-    name: "Miron Vitold",
-    phone: "972-333-4106",
-  },
-  {
-    id: "5e887a1fbefd7938eea9c981",
-    address: {
-      city: "Berkeley",
-      country: "USA",
-      state: "California",
-      street: "317 Angus Road",
-    },
-    avatar: "https://htran844.github.io/hihi/img/ig/i35.jpg",
-    createdAt: subDays(subHours(now, 5), 4).getTime(),
-    email: "penjani.inyene@devias.io",
-    name: "Penjani Inyene",
-    phone: "858-602-3409",
-  },
-  {
-    id: "5e887d0b3d090c1b8f162003",
-    address: {
-      city: "Carson City",
-      country: "USA",
-      state: "Nevada",
-      street: "2188  Armbrester Drive",
-    },
-    avatar: "https://htran844.github.io/hihi/img/ig/i31.jpg",
-    createdAt: subDays(subHours(now, 15), 4).getTime(),
-    email: "omar.darobe@devias.io",
-    name: "Omar Darobe",
-    phone: "415-907-2647",
-  },
-  {
-    id: "5e88792be2d4cfb4bf0971d9",
-    address: {
-      city: "Los Angeles",
-      country: "USA",
-      state: "California",
-      street: "1798  Hickory Ridge Drive",
-    },
-    avatar: "https://htran844.github.io/hihi/img/ig/i22.jpg",
-    createdAt: subDays(subHours(now, 2), 5).getTime(),
-    email: "siegbert.gottfried@devias.io",
-    name: "Siegbert Gottfried",
-    phone: "702-661-1654",
-  },
-  {
-    id: "5e8877da9a65442b11551975",
-    address: {
-      city: "Murray",
-      country: "USA",
-      state: "Utah",
-      street: "3934  Wildrose Lane",
-    },
-    avatar: "https://htran844.github.io/hihi/img/ig/i26.jpg",
-    createdAt: subDays(subHours(now, 8), 6).getTime(),
-    email: "iulia.albu@devias.io",
-    name: "Iulia Albu",
-    phone: "313-812-8947",
-  },
-  {
-    id: "5e8680e60cba5019c5ca6fda",
-    address: {
-      city: "Salt Lake City",
-      country: "USA",
-      state: "Utah",
-      street: "368 Lamberts Branch Road",
-    },
-    avatar: "https://htran844.github.io/hihi/img/ig/i28.jpg",
-    createdAt: subDays(subHours(now, 1), 9).getTime(),
-    email: "nasimiyu.danai@devias.io",
-    name: "Nasimiyu Danai",
-    phone: "801-301-7894",
-  },
-];
-
-const useCustomers = (page: any, rowsPerPage: any) => {
-  return useMemo(() => {
-    return applyPagination(data, page, rowsPerPage);
-  }, [page, rowsPerPage]);
-};
-
-const useCustomerIds = (customers: any) => {
-  return useMemo(() => {
-    return customers.map((customer: any) => customer.id);
-  }, [customers]);
-};
-
 const Customer = () => {
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const customers = useCustomers(page, rowsPerPage);
-  const customersIds = useCustomerIds(customers);
-  const customersSelection = useSelection(customersIds);
+
+  const { data: users } = useGetUser();
 
   const handlePageChange = useCallback((event: any, value: any) => {
     setPage(value);
@@ -145,6 +28,8 @@ const Customer = () => {
   const handleRowsPerPageChange = useCallback((event: any) => {
     setRowsPerPage(event.target.value);
   }, []);
+
+  const [data, setData] = useState([]);
 
   return (
     <Box
@@ -196,19 +81,13 @@ const Customer = () => {
             </div>
           </Stack>
           <CustomersSearch />
-          <CustomersTable
+          {/* <CustomersTable
             count={data.length}
             items={customers}
-            onDeselectAll={customersSelection.handleDeselectAll}
-            onDeselectOne={customersSelection.handleDeselectOne}
             onPageChange={handlePageChange}
             onRowsPerPageChange={handleRowsPerPageChange}
-            onSelectAll={customersSelection.handleSelectAll}
-            onSelectOne={customersSelection.handleSelectOne}
             page={page}
-            rowsPerPage={rowsPerPage}
-            selected={customersSelection.selected}
-          />
+          /> */}
         </Stack>
       </Container>
     </Box>
--- services/CustomerService.ts
+++ services/CustomerService.ts
@@ -1,3 +1,16 @@
+import { httpClient } from "@/utils";
+import { useQuery } from "@tanstack/react-query";
+
 export function applyPagination(documents: any, page: any, rowsPerPage: any) {
   return documents.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
 }
+
+const endpoint = "/users";
+export const useGetUser = () => {
+  return useQuery([endpoint], async () => {
+    const res = await httpClient().get(endpoint);
+    const { data } = res.data;
+
+    return data;
+  });
+};
