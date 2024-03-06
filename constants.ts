2eda807c559f830c574f25ff7b34aa4790317b2e
anhvv <vak63.uet.vnu@gmail.com>
2024-03-06 09:23:29 +0700

    update env firebase

--- constants.ts
+++ constants.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyA5kjF31z1CpgvkcQtUEDy95UXC7xc4fs0",
-  authDomain: "zinza-ffdb1.firebaseapp.com",
-  projectId: "zinza-ffdb1",
-  storageBucket: "zinza-ffdb1.appspot.com",
-  messagingSenderId: "395299467259",
-  appId: "1:395299467259:web:5d74d79f0b1644ce1e255e",
-  measurementId: "G-7G8HH1SV55",
+  apiKey: "AIzaSyB1sWo0XAQgtSLUrinsCmnD23kTlPm5W0Q",
+  authDomain: "zorder-d2a30.firebaseapp.com",
+  projectId: "zorder-d2a30",
+  storageBucket: "zorder-d2a30.appspot.com",
+  messagingSenderId: "898190643438",
+  appId: "1:898190643438:web:67d9056a584f0d01bce3ff",
+  measurementId: "G-ZJ2WGKMSKD",
 };
